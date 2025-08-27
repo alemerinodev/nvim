34df601ec3cf7d519fae74323653737c1ecc0427
Alejandro Merino <ale.merino.rosales@gmail.com>
2025-08-27 10:45:15 +0200

    Fix markdown-preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,7 +2,11 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = function()
+        vim.cmd [[Lazy load markdown-preview.nvim]]
+        vim.fn["mkdp#util#install"]()
+    end,
+
     keys = {
         { "<leader>md", "<CMD>MarkdownPreviewToggle<CR>", desc = "Show markdown preview" },
     }
